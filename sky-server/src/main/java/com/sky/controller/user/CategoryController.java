5ae6799f28a99f43b1384826e9f2d0a372a88d27
Ethan <2553035342@qq.com>
2023-12-08 16:53:28 +0800

    用户端分类查询业务实现

---
+++ sky-server/src/main/java/com/sky/controller/user/CategoryController.java
@@ -0,0 +1,27 @@
+package com.sky.controller.user;
+
+import com.sky.entity.Category;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("userCategoryController")
+@Slf4j
+@RequestMapping("/user/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+    @GetMapping("/list")
+    public Result<List<Category>> getCategoryByType(Integer type){
+        log.info("根据type获取分类，type：{}",type);
+        List<Category> categories = categoryService.typeQuery(type);
+        return Result.success(categories);
+    }
+}
