ff56d3a435c9ee439b6a9504b4cf7504575cd7c4
Ethan <2553035342@qq.com>
2023-12-10 12:14:27 +0800

    管理端查询订单明细代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -4,12 +4,10 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController("adminOrderController")
 @RequestMapping("/admin/order")
@@ -29,5 +27,18 @@ public class OrderController {
         log.info("管理端订单搜索");
         PageResult pageResult=orderService.pageQuery(ordersPageQueryDTO);
         return Result.success(pageResult);
+    }
+
+
+    /**
+     * 管理端查询订单明细
+     * @param id
+     * @return
+     */
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> getOrderDetailById(@PathVariable Long id){
+        log.info("管理端查询订单明细，订单id：{}",id);
+        OrderVO orderVO = orderService.getOrderDetailById(id);
+        return Result.success(orderVO);
     }
 }
