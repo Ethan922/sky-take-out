8e4b139ee350e37db18ef8602c814ed211713181
Ethan <2553035342@qq.com>
2023-12-16 19:44:06 +0800

    引入mybatis-plus依赖

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -1,5 +1,4 @@
 package com.sky.mapper;
-
 import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -8,15 +7,13 @@ import java.util.List;
 import java.util.Map;
 
 @Mapper
-public interface UserMapper {
+public interface UserMapper{
 
     void insert(User user);
 
     @Select("select * from user where openid=#{openid};")
     User selectByOpenid(String openid);
 
-    @Select("select * from user;")
-    List<User> getAllUsers();
 
     @Select("select * from user where id=#{userId};")
     User getById(Long userId);
